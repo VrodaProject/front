0bb5924503e739974645df7be3842991970933d3
Sin-god <illya.komarnickii1@gmail.com>
2024-03-26 20:16:24 +0200

    fix image

--- src/modules/main/components/slide/slide.component.tsx
+++ src/modules/main/components/slide/slide.component.tsx
@@ -5,7 +5,7 @@ interface SlideProps {}
 export const Slide: FC<SlideProps> = ({}) => {
   return (
     <div className="relative -top-12 w-full h-150 ">
-      <div className="bg-[url('@app/assets/images/slide.png')] bg-no-repeat bg-cover bg-center w-full h-full">
+      <div className="bg-[url('@app/assets/images/slide.jpg')] bg-no-repeat bg-cover bg-center w-full h-full">
         <div className="flex flex-col justify-center items-center h-full">
           <h1 className="text-white text-7xl mb-2 text-center sm:text-left">
             КЛУБ КРАСИ VR<span className="text-darkTea">O</span>DA
